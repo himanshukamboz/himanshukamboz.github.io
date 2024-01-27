0a2e2a3f2eabb1b2b4aea7912a6b4082e103eb7b
himanshukamboz <125533582+himanshukamboz@users.noreply.github.com>
2024-01-27 22:58:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('simg').addEventListener('click', function() {
+            // Toggle the class to show/hide the navigation
+            document.querySelector('header').classList.toggle('nav-visible');
+        });
+    
